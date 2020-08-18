630a2119f00995eecb5768762a42b27bb28f5c3a
DarthHamza <hamzamakia@gmail.com>
2020-08-18 13:12:15 +0300

    finished task

--- cashier.py
+++ cashier.py
@@ -1,5 +1,28 @@
 def main():
-	# write code here
+	cart = []
+
+	item = input("Item (Type \"done\" to exit: ")
+
+	while item != "done":
+		price = input("Price: ")
+		qty = input("Qty: ")
+
+		cart.append({"name": item, "price": float(price), "quantity": int(qty)})
+		item = input("Item (Type \"done\" to exit: ")
+
+	print("\n")
+	print("-"*40)
+	print("Fatoura")
+	print("-"*40)
+
+	total = 0
+	for item in cart:
+		print(f"{item['quantity']} - {item['name']} - {item['quantity'] * item['price']}")
+		total += item['quantity'] * item['price']
+
+	print("-"*40)
+
+	print(f"Total: ${total}")
 
 if __name__ == '__main__':
 	main()
